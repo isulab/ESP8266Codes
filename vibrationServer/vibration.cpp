5b990c6281f549fd9a5796f38be9ab63ca1754de
imoneg <kazu.takishima@gmail.com>
2017-10-24 20:32:46 +0900

    "Huh, a quantumly-unobservable cat... Until it's observed, it's both alive and dead. Just like Schrödinger's Cat." "Although since I observed it, that determined it was alive."

--- vibrationServer/vibration.cpp
+++ vibrationServer/vibration.cpp
@@ -4,34 +4,41 @@ Vibration::Vibration(){
   //strength,interval,duty
   this->setting(0.8,100,0.5);
   pin = 0;
+  state = OFF;
 }
 Vibration::Vibration(int pin):pin(pin){
   this->setting(0.8,100,0.5);
+  state = OFF;
 }
 void Vibration::setting(float _strength, int _interval, float duty){
-  strength = _strength;
-  interval = _interval;
-  ratio = duty;
+  strength = _strength < 0.0 ? 0.0 : _strength > 1.0 ? 1.0 : _strength;
+  interval = (unsigned int)_interval;
+  ratio = duty < 0.0 ? 0.0 : duty > 1.0 ? 1.0 : duty;
   pwm_duty = (int)(1024.0 * strength);
   on_time = (int)(ratio * (float)interval);
   off_time = interval - on_time;
   toggle_time = 0;
+  //Serial.println(String() + "ratio : " + ratio + ",pwm : " + pwm_duty + ",on : " + on_time + ",off : " + off_time);
 }
 
 void Vibration::update(){
+  //Serial.println(state);
   if(state == OFF)return;
   int now_time = millis();
+  //Serial.println(String() + "now : " + now_time);
   if(toggle_time <= now_time){
     switch(state){
       case ON: //オンだったらオフにする
         analogWrite(pin,0);
         toggle_time = now_time + off_time;
         state = WAIT;
+        //Serial.println("OFF");
         break;
       case WAIT: //オフだったらオンにする
         analogWrite(pin,pwm_duty);
         toggle_time = now_time + on_time;
         state = ON;
+        //Serial.println("ON");
         break;
     }
   }
--- vibrationServer/vibrationServer.ino
+++ vibrationServer/vibrationServer.ino
@@ -6,52 +6,18 @@
 #define WIFI_PWD "mokemoke"
 
 ESP8266WebServer server(80);
-Ticker ticker1;
-Ticker led_loop_ticker;
-int led_pwm_duty = 300; //PWMのduty比 0~1023(brightness)
-int led_duty = 127; //LEDの点滅duty比(0~255)
-int led_interval = 500; //LEDの点滅周期(0~65535ms)
+Ticker loop_ticker;
+Vibration v0(5),v1(12);
 // HTML
 #define HTML_HEADER "<!doctype html>"\
   "<html><head><meta charset=\"UTF-8\"/>"\
   "<meta name=\"viewport\" content=\"width=device-width\"/>"\
   "</head><body>"
 #define HTML_FOOTER "</body></html>"
-void L_chika(){
-  //Serial.println("I am L chika");
-  static bool toggle = false;
-  if(toggle){
-    digitalWrite(2,HIGH);
-    toggle = false;
-  }else{
-    digitalWrite(2,LOW);
-    toggle = true;
-  }
-}
-void led_tiemr_interrupt(){
-  static bool led_state = false; //true:on false:off
-  static unsigned int toggle_time = 0;
-  static float ratio = (float)led_duty / (float)255;
-  static int on_time = (int)(ratio * (float)led_interval); 
-  static int off_time = led_interval - on_time;
-  
-  int now_time = millis();
-  //Serial.println(now_time);
-  //ledの状態を変更すべき時間が来たら
-  if(toggle_time <= now_time){
-    if(led_state){ //onだったらoffにする
-      analogWrite(5,0);
-      toggle_time = now_time + off_time;
-      led_state = false;
-    }else{
-      analogWrite(5,led_pwm_duty);
-      toggle_time = now_time + on_time;
-      led_state = true;
-    }
-    ratio = (float)led_duty / (float)255;
-    on_time = (int)(ratio * (float)led_interval);
-    off_time = led_interval - on_time;
-  }
+void tiemr_interrupt(){
+  Serial.println("int");
+  v0.update();
+  v1.update();
 }
 void setup() {
   pinMode(5,OUTPUT);
@@ -72,39 +38,70 @@ void setup() {
 
   // Setup WebServer Handlers
   server.on("/", [](){
-    String html = HTML_HEADER "<h1>I am the vibration server.</h1>" HTML_FOOTER;
+    String html = HTML_HEADER "<h1>I am vibrations server.</h1>" HTML_FOOTER;
     server.send(200, "text/html", html);
   });
 
   server.on("/vibration/0/on", [](){
-    String html = HTML_HEADER "<h1>Vibration ON</h1>" HTML_FOOTER;
+    float duty = 0.5;
+    float pwm_duty = 0.8;
+    int interval = 100;
+    String html = HTML_HEADER "<h1>Vibration 0 ON</h1>" HTML_FOOTER;
     server.send(200, "text/html", html);
     for(int i = 0; i < server.args(); i ++){
-      Serial.println("Arg" + (String)i + "->" + server.argName(i) + ":" + server.arg(i));
+      //Serial.println("Arg" + (String)i + "->" + server.argName(i) + ":" + server.arg(i));
       if(server.argName(i).compareTo("duty") == 0){
-        led_duty = server.arg(i).toInt();
+        duty = server.arg(i).toFloat();
       }
       if(server.argName(i).compareTo("strength") == 0){
-        led_pwm_duty = server.arg(i).toInt();
+        pwm_duty = server.arg(i).toFloat();
       }
       if(server.argName(i).compareTo("interval") == 0){
-        led_interval = server.arg(i).toInt();
+        interval = server.arg(i).toInt();
       }
     }
-    //ticker1.attach_ms(500,L_chika);
-    //digitalWrite(2,HIGH);
-    //analogWrite(2,led_pwm_duty);
-    led_loop_ticker.attach_ms(1,led_tiemr_interrupt);
+    v0.setting(pwm_duty,interval,duty);
+    v0.on();
+    loop_ticker.attach_ms(10,tiemr_interrupt);
   });
-
-  server.on("/vibration/0/off", [](){
-    String html = HTML_HEADER "<h1>Vibration OFF</h1>" HTML_FOOTER;
+  server.on("/vibration/1/on", [](){
+    float duty = 0.5;
+    float pwm_duty = 0.8;
+    int interval = 100;
+    String html = HTML_HEADER "<h1>Vibration 1 ON</h1>" HTML_FOOTER;
     server.send(200, "text/html", html);
-    led_loop_ticker.detach();
-    //digitalWrite(2,LOW);
-    analogWrite(5,0);
+    for(int i = 0; i < server.args(); i ++){
+      //Serial.println("Arg" + (String)i + "->" + server.argName(i) + ":" + server.arg(i));
+      if(server.argName(i).compareTo("duty") == 0){
+        duty = server.arg(i).toFloat();
+      }
+      if(server.argName(i).compareTo("strength") == 0){
+        pwm_duty = server.arg(i).toFloat();
+      }
+      if(server.argName(i).compareTo("interval") == 0){
+        interval = server.arg(i).toInt();
+      }
+    }
+    v1.setting(pwm_duty,interval,duty);
+    v1.on();
+    loop_ticker.attach_ms(10,tiemr_interrupt);
   });
-
+  server.on("/vibration/0/off", [](){
+    String html = HTML_HEADER "<h1>Vibration 0 OFF</h1>" HTML_FOOTER;
+    server.send(200, "text/html", html);
+    v0.off();
+    if(v0.state == OFF && v1.state == OFF ){
+      loop_ticker.detach();
+    }
+  });
+  server.on("/vibration/1/off", [](){
+    String html = HTML_HEADER "<h1>Vibration 1 OFF</h1>" HTML_FOOTER;
+    server.send(200, "text/html", html);
+    v1.off();
+    if(v0.state == OFF && v1.state == OFF ){
+      loop_ticker.detach();
+    }
+  });
   server.begin();
 }
 
